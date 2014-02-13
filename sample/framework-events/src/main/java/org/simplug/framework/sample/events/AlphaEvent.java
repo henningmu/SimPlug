b1f9e0b045c8257280d41ccc59ec9a03d98db5ee
henningmu <sicksta@gmail.com>
2014-02-13 11:16:44 +0100

    Added no-op Sample Events

---
+++ sample/framework-events/src/main/java/org/simplug/framework/sample/events/AlphaEvent.java
@@ -0,0 +1,9 @@
+package org.simplug.framework.sample.events;
+
+import org.simplug.framework.model.Event;
+
+public class AlphaEvent extends Event {
+	public AlphaEvent() {
+
+	}
+}
---
+++ sample/framework-events/src/main/java/org/simplug/framework/sample/events/BetaEvent.java
@@ -0,0 +1,9 @@
+package org.simplug.framework.sample.events;
+
+import org.simplug.framework.model.Event;
+
+public class BetaEvent extends Event {
+	public BetaEvent() {
+
+	}
+}
